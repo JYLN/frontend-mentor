fa423e0b18ceb629a3a2e11e569467fbb4ec6db3
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-07-31 15:48:22 -0600

    :art: Setup component typography and colors

--- blog-preview-card-challenge/tailwind.config.js
+++ blog-preview-card-challenge/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      yellow: "#F4D04E",
+      gray: {
+        500: "#6B6B6B",
+        950: "#111111",
+      },
+      white: "#FFFFFF",
+    },
+    spacing: {
+      50: "4px",
+      100: "8px",
+      150: "12px",
+      200: "24px",
+    },
+    extend: {
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
