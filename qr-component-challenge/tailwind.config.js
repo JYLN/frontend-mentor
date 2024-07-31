3393d0e056e1583118dfacf62c4ff9b87af88bee
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-07-31 11:17:08 -0600

    :art: Setup component typography and colors

--- qr-component-challenge/tailwind.config.js
+++ qr-component-challenge/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.html'],
   theme: {
-    extend: {},
+    colors: {
+      blue: {
+        500: '#3685FF',
+        600: '#2C7DFA',
+      },
+      slate: {
+        300: '#D5E1EF',
+        500: '#6877BD',
+        900: '#1F314F',
+      },
+      white: '#FFFFFF',
+    },
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
